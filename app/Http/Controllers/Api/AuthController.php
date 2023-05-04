bba373ee4747b7c93a22d3d4205e6dfc5f745ac3
Soouyette <elliot.leguen@gmail.com>
2023-05-04 14:50:52 +0200

    password

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -34,9 +34,8 @@ class AuthController extends Controller
                         'required',
                         'min:6',
                         'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
-                        'confirmed'
                     ],
-                    'password2' => 'required|same:password'
+                    'password2' => 'required'
                 ]);
 
             if($validateUser->fails()){
